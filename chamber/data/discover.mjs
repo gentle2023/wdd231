b21c4aba6a781b6a8e259f3c79d871eecdc100a4
John Mark <iani@byupathway.edu>
2026-04-02 21:36:45 +0100

    added discover page

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,56 @@
+export const itemsOfInterest = [
+    {
+name: "Numan Hills",
+address: "Numan, Akwanga",
+image: "images/numan.webp",
+description: "A large nature reserve famous for its hills, wildlife and peaceful environment."
+},
+
+{
+name: "Amin Zaigi Art Gallery",
+address: "No. 212 Keffi Road, Akwanga",
+image: "images/amin.webp",
+description: "One of the largest art galleries in Nigeria showcasing Mada culture and artwork."
+},
+
+{
+name: "Nasarawa Silos",
+address: "Gwanje, Akwanga",
+image: "images/silos.webp",
+description: "A storage facility for grains and agricultural products, symbolizing the region's agricultural heritage."
+},
+
+{
+name: "JK Palms Park",
+address: "Wamba Road, Akwanga",
+image: "images/palm.webp",
+description: "A peaceful park offering recreational facilities and a serene environment."
+},
+
+{
+name: "ERCC Medical Centre",
+address: "Alushi, Alkwanga",
+image: "images/alushi.webp",
+description: "A leading medical facility providing comprehensive healthcare services."
+},
+
+{
+name: "College of Education",
+address: "Andaha Road, Akwanga",
+image: "images/college.webp",
+description: "A leading educational institution offering various academic programs and research opportunities."
+},
+
+{
+name: "Chun Mada Palace",
+address: "Keffi Road, Akwanga",
+image: "images/mada.webp",
+description: "The historic palace of the Mada people, showcasing traditional architecture and cultural heritage."
+},
+{
+name: "Light House Resort",
+address: "Gburkun, Akwanga",
+image: "images/resort.webp",
+description: "A lively private resort popular for entertainment, dining and nightlife."
+}
+];

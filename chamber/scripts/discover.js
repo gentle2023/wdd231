ac5967e3ba2e9cebd4c360021853b146c2602668
John Mark <iani@byupathway.edu>
2026-04-07 13:38:01 +0100

    update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -46,3 +46,12 @@ if (!lastVisit) {
     }
 }
 localStorage.setItem("lastVisit", now);
+
+// JavaScript for Hamburger
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  hamburger.textContent = nav.classList.contains("open") ? "✖" : "☰";
+});
